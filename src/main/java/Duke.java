9ae3ee5409f8633bbdf98cca76c35d57701d9899
weng-kexin <weng.kexin98@gmail.com>
2019-08-28 10:45:54 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,10 +10,11 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        //Level 1 echo
         String horzline = "____________________________________________________________ \n";
         System.out.println(horzline + "Hello! I'm Duke \n" + "What can I do for you? \n" + horzline);
 
+        String[] myArray = new String[100];
+        int i = 1;
         boolean br = false;
         while (!br) {
             Scanner scanner = new Scanner(System.in);
@@ -21,8 +22,16 @@ public class Duke {
             if (input.equals("bye")) { //cant use "==" to compare strings **find out why
                 br = true;
                 System.out.println(horzline + "Bye. Hope to see you again soon!\n" + horzline);
-            } else {
-                System.out.println(horzline + input + "\n" + horzline);
+            } else if (input.equals("list")) { //list the array
+                System.out.println(horzline);
+                for (int j = 1; j < i; j++) {
+                    System.out.println(j + ". " + myArray[j]);
+                }
+                System.out.println(horzline);
+            } else { //add to array
+                myArray[i] = input;
+                i++;
+                System.out.println(horzline + "added: " + input + "\n" + horzline);
             }
         }
     }
